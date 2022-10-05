fcd457dea2c7aa6f59a1f713eb032cc041d0190f
Moeen M <mamapam.dev@gmail.com>
2022-10-05 16:26:45 -0400

    fix(navbar): navbar styling

--- client/components/TheNav.vue
+++ client/components/TheNav.vue
@@ -1,10 +1,10 @@
 <template>
   <header>
     <nav
-      class="bg-primary-blue flex justify-between items-center font-main h-20 px-8"
+      class="bg-primary-blue flex flex-col md:flex-row justify-between items-center font-main h-28 md:h-20 px-8 py-1"
     >
-      <h1 class="text-white text-xl">Quran Duas</h1>
-      <div class="h-full">
+      <h1 class="text-white text-lg leading-8">Quran Duas</h1>
+      <div class="h-20">
         <img
           src="../assets/images/bismillah.png"
           alt="Bismillah"
