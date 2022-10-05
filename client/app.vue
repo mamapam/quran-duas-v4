9be0662b4f9917e77420a90dd7c1aa731f7d6ce4
Moeen M <mamapam.dev@gmail.com>
2022-10-05 16:19:40 -0400

    feat(nav): create navbar

--- client/app.vue
+++ client/app.vue
@@ -1,10 +1,5 @@
 <template>
-  <div>
-    <header>
-      <nav class="bg-primary-blue flex flex-col justify-around items-center">
-        <h1>Quran Duas</h1>
-        <img src="./assets/images/bismillah.png" alt="Bismillah" />
-      </nav>
-    </header>
+  <div class="h-screen">
+    <the-nav />
   </div>
 </template>
---
+++ client/components/TheNav.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <nav
+      class="bg-primary-blue flex justify-between items-center font-main h-20 px-8"
+    >
+      <h1 class="text-white text-xl">Quran Duas</h1>
+      <div class="h-full">
+        <img
+          src="../assets/images/bismillah.png"
+          alt="Bismillah"
+          class="max-h-full md:display"
+        />
+      </div>
+    </nav>
+  </header>
+</template>
