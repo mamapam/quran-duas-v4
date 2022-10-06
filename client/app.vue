136a6bde4a031119aae7bb5a2168085188c49be3
Moeen M <mamapam.dev@gmail.com>
2022-10-06 08:45:32 -0400

    fix: rollback pinia changes

--- client/app.vue
+++ client/app.vue
@@ -1,21 +1,5 @@
-<script setup lang="ts">
-import { onMounted } from 'vue';
-import { useSurahStore } from './stores/SurahStore';
-
-const surahStore = useSurahStore();
-
-onMounted(() => {
-  setTimeout(() => {
-    surahStore.fetch();
-  }, 2000);
-  //surahStore.fetch();
-  // console.log(surahStore.getSurahList);
-});
-</script>
-
 <template>
   <div class="h-screen">
     <the-nav />
-    {{ surahStore.getSurahList }}
   </div>
 </template>
