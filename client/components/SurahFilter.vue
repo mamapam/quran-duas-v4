2ddbe96aeaf0350344aa4ddc4471dd72a850444e
Moeen M <mamapam.dev@gmail.com>
2022-10-06 18:45:20 -0400

    fix(filter): additional styling

--- client/components/SurahFilter.vue
+++ client/components/SurahFilter.vue
@@ -7,11 +7,38 @@ const surahData: SurahResponse[] = surahStore.getSurahList;
 </script>
 
 <template>
-  <div class="h-[25%] overflow-scroll bg-gray-200 p-4 overflow-x-hidden">
-    <ul class="flex flex-col flex-wrap max-h-160 sm:max-h-106 gap-4">
+  <div
+    class="h-[25%] overflow-scroll bg-gray-200 p-4 overflow-x-hidden m-2 rounded-md font-body text-sm"
+  >
+    <div class="flex justify-between pb-4 items-center">
+      <p class="font-bold text-sm pb-2 uppercase tracking-widest">Filter</p>
+      <button
+        class="bg-transparent hover:bg-primary-blue font-semibold hover:text-white py-1 px-2 border-2 border-primary-blue hover:border-transparent rounded tracking-widest uppercase"
+      >
+        RESET
+      </button>
+    </div>
+    <ul
+      class="flex flex-col flex-wrap items-start max-h-160 sm:max-h-106 gap-4"
+    >
       <li v-for="surah in surahData" :key="surah.id">
-        <input type="checkbox" />{{ surah.surah.englishName }}
+        <p class="text-left">
+          <label :for="surah.id.toString()">
+            <input
+              type="checkbox"
+              class="align-bottom relative -top-1 mr-1"
+              :id="surah.id.toString()"
+            />
+            {{ surah.surah.englishName }}
+          </label>
+        </p>
       </li>
     </ul>
   </div>
 </template>
+
+<style scoped>
+input[type='checkbox'] {
+  transform: scale(1.5);
+}
+</style>
--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
   googleFonts: {
     families: {
       'EB Garamond': true,
+      Lato: true,
     },
     preload: true,
   },
