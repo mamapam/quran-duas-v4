8079dc9a745ca5e2d8c75074bab53f8ad009df54
Moeen M <mamapam.dev@gmail.com>
2022-10-06 16:53:33 -0400

    feat(filter): add filter component

--- client/app.vue
+++ client/app.vue
@@ -12,6 +12,29 @@ onServerPrefetch(async () => {
 <template>
   <div class="h-screen">
     <the-nav />
-    {{ surahStore.getSurahList }}
+    <surah-filter />
   </div>
 </template>
+
+<style>
+::-webkit-scrollbar {
+  width: 0.5rem;
+  height: 1rem;
+}
+::-webkit-scrollbar-track {
+  background: rgb(158, 167, 158);
+}
+::-webkit-scrollbar-thumb {
+  background: rgb(70, 73, 70);
+}
+::-webkit-scrollbar-thumb:hover {
+  background: rgba(35, 36, 35, 0.5);
+}
+
+@supports (scrollbar-color: red blue) {
+  * {
+    scrollbar-color: rgb(70, 73, 70) rgb(158, 167, 158);
+    scrollbar-width: thin;
+  }
+}
+</style>
---
+++ client/components/SurahFilter.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useSurahStore } from '~~/stores/SurahStore';
+import { SurahResponse } from '~~/types';
+
+const surahStore = useSurahStore();
+const surahData: SurahResponse[] = surahStore.getSurahList;
+</script>
+
+<template>
+  <div class="h-[25%] overflow-scroll bg-gray-200 p-4 overflow-x-hidden">
+    <ul class="flex flex-col flex-wrap max-h-160 sm:max-h-106 gap-4">
+      <li v-for="surah in surahData" :key="surah.id">
+        <input type="checkbox" />{{ surah.surah.englishName }}
+      </li>
+    </ul>
+  </div>
+</template>
